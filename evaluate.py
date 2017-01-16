c2b0887ece30163788aee05451cd4275754f7177
mattiaspalmgren <mattiaspalmgren92@gmail.com>
2017-01-16 16:29:49 +0100

    similar evaluation prints for the svm

--- evaluate.py
+++ evaluate.py
@@ -3,7 +3,7 @@ from sklearn.metrics import zero_one_loss, precision_score, recall_score
 import collections
 
 
-def evaluate_classifier(classifier, test_reviews):
+def evaluate_naivebayes(classifier, test_reviews):
     # For calculating precision/recall
     ref_set = collections.defaultdict(set)
     test_set = collections.defaultdict(set)
@@ -23,17 +23,33 @@ def evaluate_classifier(classifier, test_reviews):
         ref_set_arr.append(label)
         test_set_arr.append(observed)
 
-    print('precision:', precision_score(ref_set_arr, test_set_arr))
-    print('recall:', recall_score(ref_set_arr, test_set_arr))
-    # print('neg precision:', precision(ref_set['neg'], test_set['neg']))
-    # print('pos precision:', precision(ref_set['pos'], test_set['pos']))
-    # print('neg recall:', recall(ref_set['neg'], test_set['neg']))
-    # print('pos recall:', recall(ref_set['pos'], test_set['pos']))
+    print('pos precision:', precision(ref_set['pos'], test_set['pos']))
+    print('pos recall:', recall(ref_set['pos'], test_set['pos']))
+    print('neg precision:', precision(ref_set['neg'], test_set['neg']))
+    print('neg recall:', recall(ref_set['neg'], test_set['neg']))
     print('misclassification rate', zero_one_loss(ref_set_arr, test_set_arr))
-    # print('most informative features', classifier.show_most_informative_features(10))
+    print('most informative features', classifier.show_most_informative_features(10))
 
 
-def compute_evaluation_metrics(ref_set, test_set):
-    print('precision:', precision_score(ref_set, test_set))
-    print('recall:', recall_score(ref_set, test_set))
-    print('misclassification rate', zero_one_loss(ref_set, test_set))
+def evaluate_svm(y_hat, test_reviews):
+    ref_set = collections.defaultdict(set)
+    test_set = collections.defaultdict(set)
+    ref_set_arr = []
+    test_set_arr = []
+    for i, label in enumerate(test_reviews):
+        label = "neg" if label == 0 else "pos"
+        observed = "neg" if y_hat[i] == 0 else "pos"
+        ref_set[label].add(i)
+        test_set[observed].add(i)
+
+        label = 0 if label == "neg" else 1
+        observed = 0 if observed == "neg" else 1
+        ref_set_arr.append(label)
+        test_set_arr.append(observed)
+
+    print('pos precision:', precision(ref_set['pos'], test_set['pos']))
+    print('pos recall:', recall(ref_set['pos'], test_set['pos']))
+    print('neg precision:', precision(ref_set['neg'], test_set['neg']))
+    print('neg recall:', recall(ref_set['neg'], test_set['neg']))
+    print('misclassification rate', zero_one_loss(ref_set_arr, test_set_arr))
+
