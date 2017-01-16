a2fe9b66a18e394596bbb0ae3162d5d243afe37b
mattiaspalmgren <mattiaspalmgren92@gmail.com>
2017-01-16 16:35:51 +0100

    added the lexical process methods to the process file

--- process.py
+++ process.py
@@ -1,8 +1,9 @@
-import nltk, re, json, math, random
+import nltk, re, json, math, random, numpy as np
 from nltk.corpus import stopwords
 from nltk.stem.lancaster import LancasterStemmer
 
 
+# Read documents from file
 def read_documents(filename):
     documents = []
     for line in open (filename, 'r'):
@@ -10,6 +11,7 @@ def read_documents(filename):
     return documents
 
 
+# Tokenize and stem a set of documents
 def pre_process(documents):
     document_tokens = [(nltk.word_tokenize(text), c) for (text, c) in documents]
     document_tokens = [clean_tokens(tokens, c) for (tokens, c) in document_tokens]
@@ -17,6 +19,7 @@ def pre_process(documents):
     return document_tokens
 
 
+# Remove stopwords and stem from a description
 def clean_tokens(tokens, c):
     stop = stopwords.words('english')
     stop.append('')
@@ -32,6 +35,24 @@ def clean_tokens(tokens, c):
     return tmp_tokens, c
 
 
+# Split a text into tokens
+def split(text):
+    splitter = nltk.data.load('tokenizers/punkt/english.pickle')
+    tokenizer = nltk.tokenize.TreebankWordTokenizer()
+
+    sentences = splitter.tokenize(text)
+    tokenized_sentences = [tokenizer.tokenize(sent) for sent in sentences]
+    return tokenized_sentences
+
+
+# Tag tokens with their tag depending on what kind of word it is
+def pos_tag(sentences):
+    pos = [nltk.pos_tag(sentence) for sentence in sentences]
+    pos = [[(word.lower(), [postag]) for (word, postag) in sentence] for sentence in pos]
+    return pos
+
+
+# Load the original unprocessed data
 def load_original_data(url='data/reviews_Electronics_15k.json'):
     data = read_documents(url)
 
@@ -51,6 +72,7 @@ def load_original_data(url='data/reviews_Electronics_15k.json'):
     return reviews
 
 
+# Load already processed (cleaning and tokenized) data
 def load_processed_data(url):
     with open(url, 'r') as infile:
         reviews = json.load(infile)
@@ -58,10 +80,10 @@ def load_processed_data(url):
     return reviews
 
 
+# Divide into train and test set from set of features
 def train_test_set(features):
     count = len(features)
     train_count = math.floor(0.8 * count)
-
     random.seed(12345)
     all_lines = random.sample(range(0, count), count)
     train_lines = all_lines[0:train_count]
